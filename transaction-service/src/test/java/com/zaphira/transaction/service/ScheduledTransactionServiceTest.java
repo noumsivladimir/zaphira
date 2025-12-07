da85c3eadd430c571f4d2090b6349800627daead
kingrestful <johnhamiltondread569@proton.me>
2025-12-07 13:41:59 +0100

    Common library and Kafka features

--- transaction-service/src/test/java/com/zaphira/transaction/service/ScheduledTransactionServiceTest.java
+++ transaction-service/src/test/java/com/zaphira/transaction/service/ScheduledTransactionServiceTest.java
@@ -12,7 +12,7 @@ import com.zaphira.transaction.repository.ScheduledTransactionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
+//import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
--- transaction-service/src/test/java/com/zaphira/transaction/service/TransactionServiceTest.java
+++ transaction-service/src/test/java/com/zaphira/transaction/service/TransactionServiceTest.java
@@ -23,12 +23,12 @@ import com.zaphira.transaction.service.limit.TransactionLimitService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+//import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.List;
+//import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
