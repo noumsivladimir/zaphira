e77dc61068498456f680bd59e5baae16882c1585
kingrestful <johnhamiltondread569@proton.me>
2025-12-08 12:45:02 +0100

    Kafka integration

--- wallet-service/src/main/java/com/zaphira/wallet/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/zaphira/wallet/WalletServiceApplication.java
@@ -2,10 +2,12 @@ package com.zaphira.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.zaphira.wallet", "com.zaphira.common.model.entities"})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class WalletServiceApplication {
